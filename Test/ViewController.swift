f0db50cf32d84aee6b3612ac99ca30b253176a45
Damjan <damjan@apples-imac.local>
2018-12-07 08:18:02 +0100

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Damjan on 07/12/2018.
+//  Copyright © 2018 Genesis Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
